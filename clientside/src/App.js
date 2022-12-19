be3ceb88f2304ff71884f6fd6c58bd0011223ef6
aminehz <amine.hz.hz.98@gmail.com>
2022-12-19 17:36:28 +0100

    adding routes system

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -3,19 +3,30 @@ import SignIn from './components/SignIn';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+  
      
-      <Header/>
-      bonjour 
-      bonjour
-      <Footer/>
+     <Router>
+
+<div className="App">
+<Header/>
+<Routes>
+  <Route path="/" element={<SignIn/> } />
+  
+</Routes>
+<Footer/>
+</div>
+
+     </Router>
+
+     
 
 
 
-    </div>
+    
   );
 }
 
