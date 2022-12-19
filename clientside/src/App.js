0db33431cbebf71a2c1dbec06ffcec076a265958
aminehz <amine.hz.hz.98@gmail.com>
2022-12-19 17:18:22 +0100

    layout done

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import SignIn from './components/SignIn';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
 
 function App() {
   return (
     <div className="App">
      
-
-      <SignIn/>
+      <Header/>
+      bonjour 
+      bonjour
+      <Footer/>
 
 
 
--- clientside/src/components/layouts/Header.js
+++ clientside/src/components/layouts/Header.js
@@ -1,10 +1,44 @@
-const Header = () =>{
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-return(
+const Header = () => {
+
+  return (
+    
     <div className="">
-
+      <Navbar className="NavBg">
+        <Container>
+          <Navbar.Brand id="NavLink" href="#home">
+            POLYTEC
+          </Navbar.Brand>
+          <Nav className="m-auto">
+            <Nav.Link id="NavLink" href="#home">
+              Acceuil
+            </Nav.Link>
+            <Nav.Link id="NavLink" href="#features">
+              Contact
+            </Nav.Link>
+            <NavDropdown title="Documents" id="NavLink">
+              <NavDropdown.Item href="#action/3.1">
+                Offres Des Stages
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Certification
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Document</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link id="NavLink" href="#pricing">
+              A Propos
+            </Nav.Link>
+            <Nav.Link id="NavLink" href="#pricing">
+              Sign In
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
-);
-
-}
+  );
+};
 export default Header;
