61607368bb24212713cdd7fc3f1cb44eef2a6d69
aminehz <amine.hz.hz.98@gmail.com>
2022-12-20 09:58:49 +0100

    home interface is completed

--- clientside/src/components/Acceuil.js
+++ clientside/src/components/Acceuil.js
@@ -1,9 +1,9 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-
+import Button from 'react-bootstrap/Button';
 const Acceuil = () => {
   return (
-    <div className="">
+    <div className="me-auto">
       <Carousel autoPlay interval="3000" transitionTime="5000" showThumbs={false}>
         <div className="imgCaroussel">
           <img src="assets/poly1.jpg" />
@@ -22,6 +22,10 @@ const Acceuil = () => {
          
         </div>
       </Carousel>
+      <div className="pt-5 pb-5 d-flex flex-row justify-content-center">
+      <Button id="AproposBtn">A propos</Button>
+      </div>
+    
     </div>
   );
 };
